aa6fa992714c97966ebd86af148f778dfb79ae20
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2021-11-24 10:00:10 -0800

    divider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideStorage, getStorage } from '@angular/fire/storage';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { environment } from "../environments/environment";
+import { provideStorage, getStorage } from "@angular/fire/storage";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import {
   DigestiveImageComponent,
   InfoDialogComponent,
-} from './digestive-image/digestive-image.component';
+} from "./digestive-image/digestive-image.component";
 
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { DigestionComparisonComponent } from './digestion-comparison/digestion-comparison.component';
-import { ExplanationComponent } from './explanation/explanation.component';
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatButtonModule } from "@angular/material/button";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { DigestionComparisonComponent } from "./digestion-comparison/digestion-comparison.component";
+import { ExplanationComponent } from "./explanation/explanation.component";
+
+import { MatDividerModule } from "@angular/material/divider";
 
 @NgModule({
   declarations: [
@@ -35,6 +37,7 @@ import { ExplanationComponent } from './explanation/explanation.component';
     MatDialogModule,
     MatButtonModule,
     MatTooltipModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
